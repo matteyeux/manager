057603212ac7989d667f1c61a84f90646858ee6a
matteyeux <mathieu.hautebas@gmail.com>
2017-07-12 10:46:06 +0200

    added new file for future native commands usage

---
+++ manager/common.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace commonmanager
+{
+    class commonstuff
+    {
+        public int check4file(string datfile)
+        {
+            if (File.Exists(datfile))
+                return 0;
+            else
+            {
+                return -1;
+            }
+            //Console.WriteLine(File.Exists(datfile) ? "File exists." : "File does not exist.");
+        }
+    }
+}
