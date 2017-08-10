d529a31a9fdf6980b6b67397c18086daccf3040a
matteyeux <mathieu.hautebas@gmail.com>
2017-08-10 16:46:57 +0200

    implement usb managment to detect xiring for #3

--- manager/hardware.cs
+++ manager/hardware.cs
@@ -119,5 +119,52 @@ namespace hardwaremanager
                 Console.WriteLine("An error occurred while querying for WMI data: " + e.Message);
             }
         }
+
+        public bool check_for_xiring_device()
+        {
+            var usbDevices = GetUSBDevices();
+            foreach (var usbDevice in usbDevices)
+            {
+                Debug.WriteLine("Device ID : {0}, PNP Device ID {1}, Device Description {2}", usbDevice.DeviceID, usbDevice.PnpDeviceID, usbDevice.Description);
+                if (usbDevice.PnpDeviceID == "SWD\\SCDEVICEENUM\\1_XIRING_USB_SMART_CARD_READER_0")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_PnPEntity"))
+                collection = searcher.Get();
+
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                (string)device.GetPropertyValue("DeviceID"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description")
+                ));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+    }
+    class USBDeviceInfo
+    {
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        {
+            this.DeviceID = deviceID;
+            this.PnpDeviceID = pnpDeviceID;
+            this.Description = description;
+        }
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
     }
 }
