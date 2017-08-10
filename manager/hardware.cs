4f2cfe27bc5a1d506b704a2829f3a4bf778bf080
matteyeux <mathieu.hautebas@gmail.com>
2017-08-10 13:28:50 +0200

    cleaned file

--- manager/hardware.cs
+++ manager/hardware.cs
@@ -13,38 +13,7 @@ namespace hardwaremanager
 {
     public class hardwareinfo
     {   
-        //public string check_for_disk_name(string drivename)
-        //{
-        //    if (drivename == "" || drivename == null)
-        //    {
-        //        drivename = "C";
-        //    }
-        //    return drivename;
-        //}
-
-        /// <summary>
-        /// method to retrieve the selected HDD's serial number
-        /// </summary>
-        /// <param name="strDriveLetter">Drive letter to retrieve serial number for</param>
-        /// <returns>the HDD's serial number</returns>
-        public string GetHDDSerialNumber(string drive)
-        {
-            //check to see if the user provided a drive letter
-            //if not default it to "C"
-            if (drive == "" || drive == null)
-            {
-                drive = "C";
-            }
-            //create our ManagementObject, passing it the drive letter to the
-            //DevideID using WQL
-            ManagementObject disk = new ManagementObject("Win32_LogicalDisk.DeviceID=\"" + drive + ":\"");
-            //bind our management object
-                disk.Get();
-            //return the serial number
-            return disk["VolumeSerialNumber"].ToString();
-        }
-
-        public int partition_number()
+         public int partition_number()
         {
             try
             {
@@ -64,48 +33,7 @@ namespace hardwaremanager
             }
             return 0;
         }
-   
-
-        /// <summary>
-        /// method to retrieve the HDD's freespace
-        /// </summary>
-        /// <param name="drive">Drive letter to get free space from (optional)</param>
-        /// <returns>The free space of the selected HDD</returns>
-        public double GetHDDFreeSpace(string drive)
-        {
-            //check to see if the user provided a drive letter
-            //if not default it to "C"
-            if (drive == "" || drive == null)
-            {
-                drive = "C";
-            }
-            //create our ManagementObject, passing it the drive letter to the
-            //DevideID using WQL
-            ManagementObject disk = new ManagementObject("Win32_LogicalDisk.DeviceID=\"" + drive + ":\"");
-            //bind our management object
-            disk.Get();
-            //return the free space amount
-            return Convert.ToDouble(disk["FreeSpace"]);
-        }
-
-       
-        public double getHDDSize(string drive)
-        {
-            //check to see if the user provided a drive letter
-            //if not default it to "C"
-            if (drive == "" || drive == null)
-            {
-                drive = "C";
-            }
-            //create our ManagementObject, passing it the drive letter to the
-            //DevideID using WQL
-            ManagementObject disk = new ManagementObject("Win32_LogicalDisk.DeviceID=\"" + drive + ":\"");
-            //bind our management object
-            disk.Get();
-            //return the HDD's initial size
-            return Convert.ToDouble(disk["Size"]);
-        }
-       
+               
         public void voltage_info()
         {
             System.Management.ObjectQuery query = new ObjectQuery("Select * FROM Win32_Battery");
@@ -144,26 +72,7 @@ namespace hardwaremanager
                 Console.WriteLine("An error occurred while querying for WMI data: " + e.Message);
             }
         }
-        public int bios_is_cool()
-        {
-            try
-            {
-                ManagementObjectSearcher searcher =
-                    new ManagementObjectSearcher("root\\CIMV2",
-                    "SELECT * FROM Win32_BIOS");
-
-                foreach (ManagementObject queryObj in searcher.Get())
-                {
-                    Console.WriteLine("BIOS: {0}", queryObj["Caption"]); //Just check if default BIOS / RIP backdoors
-
-                }
-            }
-            catch (ManagementException e)
-            {
-                Console.WriteLine("An error occurred while querying for WMI data: " + e.Message);
-            }
-            return 0;
-        }
+        
         public string ram_Type()
         {
                
